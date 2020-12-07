df60f4b1b3ebea50231ff6f5f0333d8dedd9338a
Makian Lel <hohosmarko@gmail.com>
2020-12-07 12:59:38 +0100

    Day001 first push

---
+++ Day001/Day1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] listOfNums = { 10, 15, 1, 7, 4, 7 };
+            int k = 14;
+
+            bool can = checkIfCan(listOfNums, k);
+
+           if(can)
+            {
+                Console.WriteLine("Possible");
+            }
+           else
+            {
+                Console.WriteLine("Not possible");
+            }
+        }
+
+        static bool checkIfCan(int[] list, int k)
+        {
+            for (int i = 0; i < list.Length - 1; ++i)
+            {
+                for (int j = i + 1; j < list.Length; ++j)
+                {
+                    if (list[i] + list[j] == k)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}

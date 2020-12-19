6ca5e5abfbd811fcd323c30158248dffd18d523b
Makian Lel <hohosmarko@gmail.com>
2020-12-19 11:14:43 +0100

    Day011

---
+++ Day011/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Day011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = { 4, 17, 32, 15 };
+            Console.Write("Nonswapped: ");
+            for (int i = 0; i < values.Length; ++i)
+            {
+                Console.Write($"{values[i]} ");
+            }
+            Console.Write("\nSwapped: ");
+            values = Swap(values);
+
+            for (int i = 0; i < values.Length; ++i)
+            {
+                Console.Write($"{values[i]} ");
+            }
+        }
+
+        static int[] Swap(int[] array)
+        {
+            for(int i = 0; i < array.Length - 1; i += 2)
+            {
+                (array[i], array[i + 1]) = (array[i + 1], array[i]);
+            }
+
+            return array;
+        }
+    }
+}

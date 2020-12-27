8b18e621b5370987cdc9ac6c4853648d49aa6715
Makian Lel <hohosmarko@gmail.com>
2020-12-27 16:58:38 +0100

    Day018

---
+++ Day018/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Day018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[6] { 2, 2, 6, 6, 6, 6 };
+
+            int[] numbers = Highest(array);
+
+            if(numbers[1] > MathF.Floor(array.Length / 2f))
+            {
+                Console.WriteLine($"Most numbers are: {numbers[0]}");
+            }
+        }
+
+        static int[] Highest(int[] arr)
+        {
+            int num = 0, tempmax = 0, maxIndex = 0;
+
+            for(int i = 0; i < arr.Length; ++i)
+            {
+                tempmax = 0;
+                for(int j = i; j < arr.Length; ++j)
+                {
+                    if(arr[j] == arr[i])
+                    {
+                        tempmax++;
+                    }
+                }
+
+                if(tempmax > num)
+                {
+                    num = tempmax;
+                    maxIndex = i;
+                }
+            }
+
+            return new int[2] { arr[maxIndex], num };
+        }
+    }
+}
